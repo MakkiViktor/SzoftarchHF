07d437c3aac0f3ff713d121eb0cc738ec849396b
Udvari Tibor <udvaritibor95@gmail.com>
2018-11-09 21:33:47 +0100

    pusholom ezt, pedig gitignoreban benne van és nem kéne itt lennie.

--- build/Test.js
+++ build/Test.js
@@ -1,30 +1,66 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Test = /** @class */ (function () {
-    function Test(ID, Permission, FirstName, LastName, DB) {
-        this.getIDSql = "SELECT * FROM user WHERE ID = " + this.id + ";";
-        this.updateSql = "UPDATE user SET FName = " + this.firstName + ", LName = " + this.lastName + ", Perm = " +
-            this.permission + ", Password = " + this.password + ", UserName = " + this.userName + " WHERE ID = " + this.id + ";";
-        this.insertSql = "INSERT INTO user (FName, LName, Perm, UserName, Password)" + "VALUES (" +
-            this.firstName + "," + this.lastName + "," + this.permission + "," + this.userName + "," + this.password + ");";
-        this.id = ID;
-        this.permission = Permission;
-        this.firstName = FirstName;
-        this.lastName = LastName;
-        this.db = DB;
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
     }
-    Test.prototype.save = function () {
-        var json;
-        var id;
-        json = this.db.execute(this.getIDSql);
-        id = json[0].ID;
-        if (id === this.id) {
-            this.db.execute(this.updateSql);
-        }
-        else
-            this.db.execute(this.insertSql);
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+var DBObject_1 = require("./DBObject");
+var Dictionary_1 = require("./Dictionary");
+var User_1 = require("./User");
+var Words_1 = require("./Words");
+var Test = /** @class */ (function (_super) {
+    __extends(Test, _super);
+    function Test(DB, Dictionary, Creator, Level) {
+        if (Dictionary === void 0) { Dictionary = null; }
+        if (Creator === void 0) { Creator = null; }
+        if (Level === void 0) { Level = null; }
+        var _this = _super.call(this, DB, "test") || this;
+        _this.dictionary = Dictionary;
+        _this.creator = Creator;
+        _this.level = _this.level;
+        _this.loadWords();
+        return _this;
+    }
+    Test.prototype.loadWords = function () {
+        var _this = this;
+        this.getMany("test_words", { name: "TestID", value: this.id, fk_table: null }).forEach(function (element) {
+            _this.getMany("Words", { name: "ID", value: element.WordID, fk_table: null }).forEach(function (element) {
+                if (_this.words.length !== 0)
+                    _this.words[_this.words.length] = new Words_1.Word(_this.db);
+                else
+                    _this.words[0] = new Words_1.Word(_this.db);
+                _this.words[_this.words.length - 1].load(element);
+            });
+        });
+        ;
+    };
+    Test.prototype.load = function (json) {
+        this.id = json['ID'];
+        this.creator = new User_1.User(this.db);
+        this.creator.load(this.getMany("users", { name: "ID", value: json['CreatorID'], fk_table: null }));
+        this.dictionary = new Dictionary_1.Dictionary(this.db);
+        this.dictionary.load(this.getMany("dictionaries", { name: "ID", value: json['DictID'], fk_table: null }));
+        this.level = json['Level'];
+        this.loadWords();
+    };
+    Test.prototype.initializeDBParams = function () {
+        this.DBparams = [
+            { name: "Name", value: this.name, fk_table: null },
+            { name: "Level", value: this.level, fk_table: null },
+            { name: "DictID", value: this.dictionary.id, fk_table: "dictionaries" },
+            { name: "CreatorID", value: this.creator.id, fk_table: "users" }
+        ];
     };
     return Test;
-}());
+}(DBObject_1.DBObject));
 exports.Test = Test;
 //# sourceMappingURL=Test.js.map
