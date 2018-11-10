2975043823d00a0b852f5de298e502fa36a3610d
Udvari Tibor <udvaritibor95@gmail.com>
2018-11-10 18:46:39 +0100

    users átnevezve users-re, és a konstruktorban szerintem user helyett course kell.

--- TypeScript/Course.ts
+++ TypeScript/Course.ts
@@ -9,18 +9,16 @@ export class Course extends DBObject{
     level : number;
 
     constructor(DB : DBContext, Teacher:User= null, Name: string= null, Level : number= null){
-        super(DB, "user");
+        super(DB, "course");
         this.teacher = Teacher;
         this.name = Name;
         this.level = Level;
         this.loadStudents();
     }
 
-    
-    //TODO:A users tábla talán user
     private loadStudents(){
         this.getMany("user_course", [{name: "CourseID", value : this.id, fk_table : null}]).forEach(element => {
-            this.getMany("user", [{name : "ID", value : element[0]['UserID'], fk_table : null}]).forEach(element => {
+            this.getMany("users", [{name : "ID", value : element[0]['UserID'], fk_table : null}]).forEach(element => {
                 if(this.students.length !== 0)
                     this.students[this.students.length] = new User(this.db);
                 else this.students[0] = new User(this.db);
@@ -28,25 +26,21 @@ export class Course extends DBObject{
             });
         });;
     }
-
-    
-    //TODO:A users tábla talán user
+  
     load(json: JSON){
         this.id = json['ID'];
         this.teacher = new User(this.db);
-        this.teacher.load(this.getMany("user", [{name : "ID", value : json['TeacherID'], fk_table : null}])[0]);
+        this.teacher.load(this.getMany("users", [{name : "ID", value : json['TeacherID'], fk_table : null}])[0]);
         this.name = json['Name'];
         this.level = json['Level'];
         this.loadStudents();
     }
 
-    
-    //TODO:A users tábla talán user
     initializeDBParams(){
         this.DBparams = [
             { name : "Name", value : this.name, fk_table : null},
             { name : "Level", value : this.level, fk_table : null},
-            { name : "TeacherID", value : this.teacher.id, fk_table : "user"}
+            { name : "TeacherID", value : this.teacher.id, fk_table : "users"}
         ]
     }
 }
