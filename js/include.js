26abff83c97552f7392baf482ed595c7101d985d
Udvari Tibor <udvaritibor95@gmail.com>
2018-11-19 18:32:54 +0100

    includes.js includeolja a minden lapnál szükséges js és css fájlokat. (<head>-be raktam, mert hülyén néz ki, hogy 1 másodperc késéssel jelenik meg a dizájn) Bootstrap használatra készen állnak a lapok. (kivéve a rég módosítottak) Index.html átrakva html mappába, hogy működjön az includes.js itt is.

--- js/include.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(){
-    $("#includes").load("../html/includes.html");
-})
---
+++ js/includes.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('head').append("<script src='https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js' integrity='sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49' crossorigin='anonymous'></script>");
+	$('head').append("<link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css' integrity='sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO' crossorigin='anonymous'>");
+	$('head').append("<script src='https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js' integrity='sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy' crossorigin='anonymous'></script>");
+
+	$('head').append("<link rel='stylesheet' type='text/css' media='screen' href='../css/main.css' />");
+	$('head').append("<script src='../js/consts.js'></script>");
+	$('head').append("<script src='../js/cookiesHandler.js'></script>");
+	$('head').append("<script src='../js/header.js'></script>");
+})
