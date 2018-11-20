07ee22069ca720a765388e01d60fc4f25813b38a
Udvari Tibor <udvaritibor95@gmail.com>
2018-11-20 11:59:16 +0100

    header magyarosítva

--- js/header.js
+++ js/header.js
@@ -14,7 +14,7 @@ var setHeader = function(){
 var loggedIn = function(){
 
     $("#rightNavBar").append("<li><b>" + getCookie(userNamec) + "</b></li>")
-    $("#rightNavBar").append("<li><a href='#' id='signout'>Sign out</a></li>");
+    $("#rightNavBar").append("<li><a href='#' id='signout'>Kijelentkezés</a></li>");
     $("#signout").on("click", function(){
         document.cookie = setCookie(userNamec, "", -1);
         alert("logged out");
@@ -23,6 +23,6 @@ var loggedIn = function(){
 }
 
 var notLoggedIn = function(){
-    $("#rightNavBar").append("<li><a href='../html/login.html'>Login</a></li>");
-    $("#rightNavBar").append("<li><a href='../html/signup.html'>Signup</a></li>");
+    $("#rightNavBar").append("<li><a href='../html/login.html'>Bejelentkezés</a></li>");
+    $("#rightNavBar").append("<li><a href='../html/signup.html'>Regisztráció</a></li>");
 }
