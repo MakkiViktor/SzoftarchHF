b50aca9ebac04a19ca3007ceb38898c0ea17d64a
Udvari Tibor <udvaritibor95@gmail.com>
2018-11-20 13:28:58 +0100

    dbcontextben van a hiba, nem érkezik meg a kérés hozzám.

--- js/DBContext.js
+++ js/DBContext.js
@@ -1,12 +1,15 @@
 var getUser = function(userName, handleData){
     let urls =  "http://tudvari.ddns.net:3000/users/";
+	console.log(urls + username);
     $.ajax({
         url: urls + userName,
         type : "GET",
-        dataType: "jsonp",
-        success: function(data ) {
-                handleData(data);
-            },
-        error: function(data) { alert(JSON.stringify(data)); },
+        dataType: "jsonp", //sima json?
+        success: function(data) {
+            handleData(data);
+        },
+        error: function(data) { 
+			alert(JSON.stringify(data)); 
+		}
     });
 }
