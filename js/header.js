ca3d9a1f59d9364256df12de03255463a2da0593
Thorvik <deldemonho@gmail.com>
2018-11-20 10:28:56 +0100

    merge conflict javítva, de megint szar lett az egész

--- js/header.js
+++ js/header.js
@@ -1,14 +1,7 @@
 
 $(document).ready(function(){
-<<<<<<< HEAD
-    $("#addheader").load("../html/header.html", "", setHeader);
-=======
-    $("#header").load("../html/header.html")
-    .ready(function(){
-        setHeader();
-    });
+    $("#header").load("../html/header.html", "", setHeader);
 
->>>>>>> 444aac029ef95174228145ac2c2c2930798d2489
 });
 
 var setHeader = function(){
@@ -19,15 +12,10 @@ var setHeader = function(){
 }
 
 var loggedIn = function(){
-<<<<<<< HEAD
-    $("#header").append("<b>" + getCookie(userNamec) + "    </b>")
-    $("#header").append("<button id = 'logoutbtn'> Log out</button>.");
-    $("#logoutbtn").on("click", function(){
-=======
+
     $("#navBarRight").append("<li><b>" + getCookie(userNamec) + "</b></li>")
     $("#navBarRight").append("<li><a href='#' id='signout'>Sign out</a></li>");
     $("#signout").on("click", function(){
->>>>>>> 444aac029ef95174228145ac2c2c2930798d2489
         document.cookie = setCookie(userNamec, "", -1);
         alert("logged out");
         location = indexHtml;  
@@ -35,11 +23,6 @@ var loggedIn = function(){
 }
 
 var notLoggedIn = function(){
-<<<<<<< HEAD
-    $("#header").append("<a href = '../html/login.html'>login     <a/>");
-    $("#header").append("<a href = '../html/signup.html'>sign up    <a/>");
-=======
     $("#navBarRight").append("<li><a href='../html/login.html'>Login</a></li>");
     $("#navBarRight").append("<li><a href='../html/signup.html'>Signup</a></li>");
->>>>>>> 444aac029ef95174228145ac2c2c2930798d2489
 }
