f42e35bce984147a84dcbdefed645c1a334855ff
Thorvik <deldemonho@gmail.com>
2018-11-23 00:32:24 +0100

    string bővítés

--- js/consts.js
+++ js/consts.js
@@ -2,7 +2,9 @@ var teacherIndexHtml = "teacherIndex.html";
 var studentIndexHtml = "studentIndex.html";
 var loginHtml = "login.html";
 var indexHtml = "../index.html";
+var noPermissionHtml = "noPermission.html"
 var userNamec = "UserName";
+var permissionc = "Permission";
 var errorc = "Error";
 
 var serverError = "server hiba";
