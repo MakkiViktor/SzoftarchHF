323832562d56622bd60efbe2e468cfb785f01a7c
Thorvik <deldemonho@gmail.com>
2018-11-23 21:17:12 +0100

    attrib]tumoknál ' cserélve "-re

--- js/teacherIndex.js
+++ js/teacherIndex.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     getUser(getCookie(userNamec), function(user){
         getTeacherCourses(user["_id"], function(data){
             for(var rows = 0; rows < data.length; rows++)
-            $("#Courses").append("<tr class='clickable-row' data-href='../html/teacherCourse.html'><th scope='row'>" + (rows + 1) + "</th><td>"+ data[rows]["Name"]+"</td><td>"+ data[rows]["Level"]+"</td></tr>");
+            $("#Courses").append("<tr class='clickable-row' data-href=\"../html/teacherCourse.html\"><th scope='row'>" + (rows + 1) + "</th><td>"+ data[rows]["Name"]+"</td><td>"+ data[rows]["Level"]+"</td></tr>");
         })
     })
 })
