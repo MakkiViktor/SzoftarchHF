6bec4066c693cfd6e0e0ad1e366ae0173b4c60b1
Thorvik <deldemonho@gmail.com>
2018-11-23 21:15:48 +0100

    Kurzus létrehozás működik

--- js/createCourse.js
+++ js/createCourse.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
         var formData = $(this).serializeArray();
         getUser(getCookie(userNamec), function (user) {
             formData[formData.length] = {
-                name: "_Creator",
+                name: "_Teacher",
                 value: user["_id"],
             }
             $.ajax({
@@ -14,9 +14,21 @@ $(document).ready(function () {
                 type: "POST",
                 dataType: "json",
                 data: formData,
-                success: function (data) { alert("Teszt létrehozva: " + data[1]) },
+                success: function (data) { 
+                    alert("Teszt létrehozva: " + data[1]) 
+                    setCookies([{
+                        name: userNamec,
+                        value: user[userNamec]
+                    },
+                    {
+                        name: courseIDc,
+                        value: data["_id"]
+                    }], 90);
+                    window.location = teacherCourseHtml;
+                },
                 error: function (data) { console.log(JSON.stringify(data)); },
             });
-        })
-
+        });
+        return false;
     });
+});
