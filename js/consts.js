a56d4f3780058845dd83d5d2bbfe392a4e162619
Thorvik <deldemonho@gmail.com>
2018-11-23 21:14:54 +0100

    userNamec javítva

--- js/consts.js
+++ js/consts.js
@@ -1,10 +1,11 @@
 var teacherIndexHtml = "teacherIndex.html";
+var teacherCourseHtml = "teacherCourse.html";
 var studentIndexHtml = "studentIndex.html";
 var loginHtml = "login.html";
 var indexHtml = "../index.html";
 var noPermissionHtml = "noPermission.html"
-var userNamec = "UserName";
-var courseIdc = "CourseId";
+var userNamec = "Username";
+var courseIDc = "CourseId";
 var permissionc = "Permission";
 var errorc = "Error";
 
