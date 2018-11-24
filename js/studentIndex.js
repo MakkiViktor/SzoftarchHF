3514d4dac73229ce04b78becb2857fbeb2948e54
Thorvik <deldemonho@gmail.com>
2018-11-24 18:27:06 +0100

    meg lett csinálva, a kurzusokat feltölti

---
+++ js/studentIndex.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    getUsersCourse(getCookie(userIDc), function(userCourses){
+        userCourses.forEach(element => {
+            getUser(element["_Teacher"], function(teacher){
+                $("#StudentCourses").append("<tr id = '" + element["_id"] +"' class=\"clickable-row\" data-href=\"../html/studentCourse.html\"><th scope=\"row\">"+ ($("#StudentCourses tr").length + 1)+"</th><td>"+ element["Name"] +"</td><td>"+ element["Level"]+"</td><td>"+ teacher["LastName"] + " " + teacher["FirstName"] +"</td></tr>");
+                $(".clickable-row").click(function() {
+                    setCookie(courseIDc, $(this).attr("id"));
+                    window.location = studentCourseHtml;
+                });
+            });
+        });
+    });
+
+    getCourses(function(courses){
+        courses.forEach(element => {
+            getUser(element["_Teacher"], function(teacher){
+                $("#AllCourses").append("<tr id = '" + element["_id"] +"' class=\"clickable-row\" data-href=\"../html/studentCourse.html\"><th scope=\"row\">"+ ($("#AllCourses tr").length + 1)+"</th><td>"+ element["Name"] +"</td><td>"+ element["Level"]+"</td><td>"+ teacher["LastName"] + " " + teacher["FirstName"] +"</td></tr>");
+                $(".clickable-row").click(function() {
+                    setCookie(courseIDc, $(this).attr("id"));
+                    window.location = studentCourseHtml;
+                });
+            });
+        });
+    });
+});
